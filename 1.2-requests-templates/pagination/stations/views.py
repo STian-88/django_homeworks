0ac4d584b2aa40d84b758aaf115d4900aecfb75e
STian-88 <s.tyan.08.1988@gmail.com>
2022-10-01 15:00:00 +0300

    solved 1.2-requests-templates-pagination

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -3,21 +3,22 @@ from django.urls import reverse
 import csv
 from django.core.paginator import Paginator
 
-with open('data-398-2018-08-30.csv', encoding='UTF-8') as csvfile:
-  reader = csv.DictReader(csvfile)
  
 def index(request):
   return redirect(reverse('bus_stations'))
 
 def bus_stations(request):
+  with open('data-398-2018-08-30.csv', encoding='UTF-8') as csvfile:
+    reader = list(csv.DictReader(csvfile))
+  
   page_number = int(request.GET.get('page', 1))
-  paginator = paginator(reader, 10)
+  paginator = Paginator(reader, 10)
   page = paginator.get_page(page_number)
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+  #  print(bas_station)
 
-  context = {
-    #     'bus_stations': ...,
-         'page': page,
-    }
+  context = { 
+  'bus_stations': paginator.page(page_number).object_list,
+  'page': paginator.page(page_number),
+  }
+
   return render(request, 'stations/index.html', context)
