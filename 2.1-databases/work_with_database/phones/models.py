7fc0dfe98676f92895e0ec53051089a09e34206f
STian-88 <s.tyan.08.1988@gmail.com>
2022-10-02 14:38:23 +0300

    solved 2.1-databases

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50, verbose_name='МОДЕЛЬ')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Стоимость')
+    image = models.TextField(verbose_name='МОДЕЛЬ')
+    release_date = models.CharField(max_length=15, verbose_name='ДАТА РЕЛИЗА')
+    lte_exists = models.BooleanField(verbose_name='LTE')
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Phone, self).save(*args, **kwargs)
+
+
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +7,23 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones_list = Phone.objects.all()
+    if request.GET.get('sort') == 'name':
+        phones_list = Phone.objects.all().order_by('-name')
+    if request.GET.get('sort') == 'min_price':
+        phones_list = Phone.objects.all().order_by('price')
+    if request.GET.get('sort') == 'max_price':
+        phones_list = Phone.objects.all().order_by('-price')
+    context = {'phones': phones_list}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    slug =' '.join(slug.split('-')) 
+    phone = Phone.objects.get(name__iexact=slug)
+    print(slug)
+    print(phone)
+    context = {'phone': phone
+    }
     return render(request, template, context)
